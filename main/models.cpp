379b9bf81c6ad19b9f1564d5347cea07bf120c38
Nicolas Barbosa <ngranato11@gmail.com>
2025-11-17 16:51:41 -0300

    teste git

--- main/models.cpp
+++ main/models.cpp
@@ -1,8 +1,9 @@
-#include <string>
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <iostream>
 using namespace std;
 
 class Usuary{
     private:
         string username;
         string password;
-}
+};
